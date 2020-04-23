6c3c81259ba5cc1a708db7dfa03e638dc217371b
Derek Kraan <derek.kraan@gmail.com>
2020-04-23 09:41:09 +0200

    Implementation

--- lib/highlander.ex
+++ lib/highlander.ex
@@ -1,18 +1,90 @@
 defmodule Highlander do
   @moduledoc """
-  Documentation for Highlander.
+  Highlander allows you to run a single process just _once_ in a cluster.
+
+  Include in your supervision tree as follow:
+
+  ```
+  children = [
+    {Highlander, child_spec}
+  ]
+  ```
+
+  `child_spec.id` is the "key" used to determine global uniqueness.
+
+  In other words, the following will generate one global process:
+
+  ```
+  children = [
+    {Highlander, %{id: :one}},
+    {Highlander, %{id: :one}}
+  ]
+  ```
+
+  The following will generate two global processes:
+
+  ```
+  children = [
+    {Highlander, %{id: :one}},
+    {Highlander, %{id: :two}}
+  ]
+  ```
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  def child_spec(child_child_spec) do
+    child_child_spec = Supervisor.child_spec(child_child_spec, [])
 
-      iex> Highlander.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    %{
+      id: child_child_spec.id,
+      start: {GenServer, :start_link, [__MODULE__, child_child_spec, []]}
+    }
   end
+
+  @impl true
+  def init(child_spec) do
+    Process.flag(:trap_exit, true)
+    {:ok, register(%{child_spec: child_spec})}
+  end
+
+  @impl true
+  def handle_info({:DOWN, ref, :process, _, _}, %{ref: ref} = state) do
+    {:noreply, register(state)}
+  end
+
+  defp name(state) do
+    %{child_spec: %{id: global_name}} = state
+    {__MODULE__, global_name}
+  end
+
+  defp register(state) do
+    case :global.register_name(name(state), self()) do
+      :yes -> start(state)
+      :no -> monitor(state)
+    end
+  end
+
+  defp start(state) do
+    {:ok, pid} = Supervisor.start_link([state.child_spec], strategy: :one_for_one)
+    %{child_spec: state.child_spec, pid: pid}
+  end
+
+  defp monitor(state) do
+    case :global.whereis_name(name(state)) do
+      :undefined ->
+        register(state)
+
+      pid ->
+        ref = Process.monitor(pid)
+        %{child_spec: state.child_spec, ref: ref}
+    end
+  end
+
+  @impl true
+  def terminate(reason, %{pid: pid}) do
+    Supervisor.stop(pid, reason)
+  end
+
+  def terminate(_, _), do: nil
 end
--- test/highlander_test.exs
+++ test/highlander_test.exs
@@ -2,7 +2,81 @@ defmodule HighlanderTest do
   use ExUnit.Case
   doctest Highlander
 
-  test "greets the world" do
-    assert Highlander.hello() == :world
+  test "runs two processes" do
+    test_pid = self()
+
+    child_spec = %{
+      start:
+        {Task, :start_link,
+         [
+           fn ->
+             send(test_pid, :hello)
+             Process.sleep(1000)
+           end
+         ]},
+      restart: :transient
+    }
+
+    Supervisor.start_link(
+      [
+        {Highlander, Map.put(child_spec, :id, :one)},
+        {Highlander, Map.put(child_spec, :id, :two)}
+      ],
+      strategy: :one_for_one
+    )
+
+    assert_receive(:hello)
+    assert_receive(:hello)
+  end
+
+  test "runs only one process" do
+    test_pid = self()
+
+    child_spec = %{
+      start:
+        {Task, :start_link,
+         [
+           fn ->
+             send(test_pid, :hello)
+             Process.sleep(1000)
+           end
+         ]},
+      restart: :transient
+    }
+
+    Supervisor.start_link(
+      [
+        {Highlander, Map.put(child_spec, :id, :one)}
+      ],
+      strategy: :one_for_one
+    )
+
+    Supervisor.start_link(
+      [
+        {Highlander, Map.put(child_spec, :id, :one)}
+      ],
+      strategy: :one_for_one
+    )
+
+    assert_receive(:hello)
+    refute_receive(:hello)
+  end
+
+  test "accepts {module, arg} child_child_spec" do
+    test_pid = self()
+
+    Supervisor.start_link(
+      [
+        {Highlander,
+         {Task,
+          fn ->
+            send(test_pid, :hello)
+            Process.sleep(1000)
+          end}}
+      ],
+      strategy: :one_for_one
+    )
+
+    assert_receive(:hello)
   end
 end
